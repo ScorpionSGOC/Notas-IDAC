89dfb5883f4ee434d8851a067061dc8e35ab67aa
ScorpionSGOC <112102532+ScorpionSGOC@users.noreply.github.com>
2023-04-18 12:41:36 -0600

    Actualizar login.php

--- login.php
+++ login.php
@@ -1,49 +1,37 @@
-<form method="post" action="guardar.php">
-  <label>Nombre:</label>
-  <input type="text" name="nombre">
-
-  <label>Email:</label>
-  <input type="email" name="email">
-
-  <button type="submit">Guardar</button>
+<form action="guardar.php" method="post">
+  <label for="nombre">Nombre:</label>
+  <input type="text" id="nombre" name="nombre"><br><br>
+  
+  <label for="apellido">Apellido:</label>
+  <input type="text" id="apellido" name="apellido"><br><br>
+  
+  <label for="email">Email:</label>
+  <input type="email" id="email" name="email"><br><br>
+  
+  <button type="submit" name="guardar">Guardar</button>
 </form>
-//
 
-<?php
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-  $nombre = $_POST['nombre'];
-  $email = $_POST['email'];
+///////
+$nombre = $_POST["nombre"];
+$apellido = $_POST["apellido"];
+$email = $_POST["email"];
 
-  // Guardar los datos en una variable o en una base de datos
+$archivo = fopen("datos.txt", "a");
+fwrite($archivo, $nombre . " " . $apellido . " - " . $email . "\n");
+fclose($archivo);
+///////
+header("Location: otra_pagina.php");
+exit;
+////////
 
-  // Redirigir a otra página para mostrar la información guardada
-  header('Location: mostrar.php');
+$contenido = file_get_contents("datos.txt");
+$lineas = explode("\n", $contenido);
+
+echo "<table>";
+foreach ($lineas as $linea) {
+    if (!empty($linea)) {
+        list($nombre, $apellido, $email) = explode(" - ", $linea);
+        echo "<tr><td>$nombre</td><td>$apellido</td><td>$email</td></tr>";
+    }
 }
-?>
-PHP:
-
-
-
-<?php
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-  $nombre = $_POST['nombre'];
-  $email = $_POST['email'];
-
-  // Guardar los datos en una variable o en una base de datos
-
-  // Redirigir a otra página para mostrar la información guardada
-  header('Location: mostrar.php');
-}
-?>
-
-/////
-<?php
-// Recuperar los datos guardados
-$nombre = 'Nombre guardado';
-$email = 'Email guardado';
-
-// Mostrar los datos en la página
-echo "<h2>Información guardada:</h2>";
-echo "<p>Nombre: $nombre</p>";
-echo "<p>Email: $email</p>";
-?>
+echo "</table>";
