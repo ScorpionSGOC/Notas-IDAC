cc2a07ba96a2e8b0e2b86c661c63bb4d003caf37
ScorpionSGOC <112102532+ScorpionSGOC@users.noreply.github.com>
2023-04-18 13:07:26 -0600

    Actualizar login.php

--- login.php
+++ login.php
@@ -1,36 +1,45 @@
-<form action="mostrar.php" method="POST">
-  Nombre: <input type="text" name="nombre"><br>
-  Email: <input type="email" name="email"><br>
-  Mensaje: <textarea name="mensaje"></textarea><br>
-  <input type="submit" value="Guardar">
+<form method="post" action="mostrar.php">
+  <label for="nombre">Nombre:</label>
+  <input type="text" id="nombre" name="nombre"><br>
+
+  <label for="email">Email:</label>
+  <input type="email" id="email" name="email"><br>
+
+  <button type="submit" id="enviar">Enviar</button>
 </form>
-
 ///
-<?php
-// Validar y sanitizar los datos recibidos por POST
-$nombre = $_POST['nombre'] ?? '';
-$email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL) ?? '';
-$mensaje = $_POST['mensaje'] ?? '';
+document.getElementById('enviar').addEventListener('click', function(event) {
+  event.preventDefault(); // Prevenir que se envíe el formulario de forma tradicional
+  
+  var nombre = document.getElementById('nombre').value;
+  var email = document.getElementById('email').value;
 
-// Guardar los datos en un archivo
-$file = fopen('datos.txt', 'a');
-fwrite($file, "$nombre | $email | $mensaje\n");
-fclose($file);
+  // Crear un objeto XMLHttpRequest para enviar los datos del formulario a la página mostrar.php
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      // Redirigir a la página mostrar.php cuando los datos se han guardado correctamente
+      window.location.href = "mostrar.php";
+    }
+  };
+  xhr.open("POST", "mostrar.php", true);
+  xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xhr.send("nombre=" + nombre + "&email=" + email);
+});
+////
+<?php
+  $nombre = $_POST['nombre'];
+  $email = $_POST['email'];
 ?>
-/////
-<table>
-  <tr>
-    <th>Nombre</th>
-    <th>Email</th>
-    <th>Mensaje</th>
-  </tr>
-  <?php
-  // Leer el archivo de datos
-  $file = fopen('datos.txt', 'r');
-  while (($line = fgets($file)) !== false) {
-    $data = explode(' | ', trim($line));
-    echo "<tr><td>$data[0]</td><td>$data[1]</td><td>$data[2]</td></tr>";
-  }
-  fclose($file);
-  ?>
-</table>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Datos del formulario</title>
+</head>
+<body>
+  <h1>Datos del formulario</h1>
+  <p>Nombre: <?php echo $nombre; ?></p>
+  <p>Email: <?php echo $email; ?></p>
+</body>
+</html>
