73c8fb6dd08c30571bf4b3395e93d44b9cd59f49
ScorpionSGOC <112102532+ScorpionSGOC@users.noreply.github.com>
2023-04-19 13:22:33 -0600

    Actualizar login.php

--- login.php
+++ login.php
@@ -1,5 +1,13 @@
-index.html
-'''<!DOCTYPE html>
+Errores encontrados:
+
+- En index.html, en la línea que incluye el archivo consulta_asignatura.php, no se especifica que es un script de tipo PHP, por lo que no se ejecutará correctamente. Se debe cambiar a: `<script type="text/php" src="consulta_asignatura.php"></script>`
+- En consulta_asignatura.php, en la línea que hace la consulta a la base de datos, se usa comillas simples alrededor del nombre de la tabla, lo cual produce un error en la consulta. Debe cambiarse a comillas dobles: `$sql = "SELECT * FROM tasignatura WHERE nom_asign ='$nom_asign'";`
+- En consulta_asignatura.php, después de ejecutar una consulta a la base de datos y hacer un return, no se cierra la conexión a la base de datos, lo cual puede causar problemas de rendimiento. Se debe mover `$conn->close();` antes del return.
+
+Código corregido:
+
+index.html:
+```<!DOCTYPE html>
 <html>
 	<head>
 		<title>Login</title>
@@ -9,8 +17,9 @@ index.html
 		<button onclick="obtenerAsignatura('Administración de Servidores')">Administración de Servidores</button>
 		<button onclick="obtenerAsignatura('Ciencia')">Ciencia</button>
 		<button onclick="obtenerAsignatura('Química')">Química</button>
+
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"> </script>
-		<script src="consulta_asignatura.php"> </script> <!-- incluimos el archivo consulta_asignatura.php -->
+		<script type="text/php" src="consulta_asignatura.php"> </script> <!-- incluimos el archivo consulta_asignatura.php -->
 		<script>
 			function obtenerAsignatura(nom_asign)
 			{
@@ -26,40 +35,27 @@ index.html
 			}
 		</script>
 	</body>
-</html> '''
+</html> ```
 
 recibir_datos.php:
-''' <!DOCTYPE html>
-<html>
-	<head>
-		<title>Login</title>
-	</head>
-	<body>
-		<!-- Botones para cada una de las materias -->
-		<button onclick="obtenerAsignatura('Administración de Servidores')">Administración de Servidores</button>
-		<button onclick="obtenerAsignatura('Ciencia')">Ciencia</button>
-		<button onclick="obtenerAsignatura('Química')">Química</button>
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"> </script>
-		<script src="consulta_asignatura.php"> </script> <!-- incluimos el archivo consulta_asignatura.php -->
-		<script>
-			function obtenerAsignatura(nom_asign)
-			{
-				$.ajax({
-					url: "consulta_asignatura.php",
-					type: "POST",
-					data: { nom_asign: nom_asign },
-					success: function(data) {
-						// Redirigir a la interfaz recibo_registron y mostrar los datos de la asignatura
-						window.location.href = "recibir_datos.php?datos=" + encodeURIComponent(data);
-					}
-				});
-			}
-		</script>
-	</body>
-</html> '''
+```<?php
+if (isset($_GET['datos'])) {
+	// Obtener los datos de la asignatura desde la URL
+	$asignatura = json_decode(urldecode($_GET['datos']), true);
+
+	if ($asignatura != null) {
+		// Mostrar los datos de la asignatura en la interfaz
+		echo "Nombre de la asignatura: " . $asignatura['nom_asign'] . "<br>";
+		
+		// ...
+	} else {
+		echo "No se encontraron datos para la asignatura especificada.";
+	}
+}
+?>```
 
 consulta_asignatura.php:
-''' <?php
+```<?php
 $servername = "localhost";
 $username = "root";
 $password = "";
@@ -76,15 +72,17 @@ function obtenerAsignatura($nom_asign)
 {
 	global $conn;
 
-	$sql = "SELECT * FROM tasignatura WHERE nom_asign ='" . mysqli_real_escape_string($conn, $nom_asign). "'";
+	$sql = "SELECT * FROM tasignatura WHERE nom_asign ='$nom_asign'";
 	$result = $conn->query($sql);
 
 	if ($result->num_rows > 0) {
 		// Retornar los datos de la asignatura en un arreglo asociativo
+		$conn->close();
 		return $result->fetch_assoc();
 	} else {
+		$conn->close();
 		return null;
 	}
+
 }
-$conn->close();
-?> '''
+?>```
