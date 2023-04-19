1c38f04758f1e06f62cf0ceef5689d0e227e0f77
ScorpionSGOC <112102532+ScorpionSGOC@users.noreply.github.com>
2023-04-19 06:48:54 -0600

    Update login.php

--- login.php
+++ login.php
@@ -1,86 +1,80 @@
-Claro, aquí te proporciono un ejemplo de cómo se verían las interfaces en HTML y cómo puedes enviar la solicitud GET al hacer clic en los botones:
-
-Página de inicio:
-```html
-<!DOCTYPE html>
+index.html
+'''<!DOCTYPE html>
 <html>
-<head>
-	<title>Página de inicio</title>
-</head>
-<body>
-	<h1>Iniciar sesión</h1>
-	<form action="validar_login.php" method="post">
-		<label for="username">Nombre de usuario:</label>
-		<input type="text" name="username" required><br><br>
-		<label for="password">Contraseña:</label>
-		<input type="password" name="password" required><br><br>
-		<input type="submit" value="Iniciar sesión">
-	</form>
-</body>
-</html>
-```
+	<head>
+		<title>Login</title>
+	</head>
+	<body>
+		<!-- Botones para cada una de las materias -->
+		<button onclick="obtenerAsignatura('Administración de Servidores')">Administración de Servidores</button>
+		<button onclick="obtenerAsignatura('Ciencia')">Ciencia</button>
+		<button onclick="obtenerAsignatura('Química')">Química</button>
 
-Página principal de la interfaz:
-```html
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Página principal</title>
-</head>
-<body>
-	<h1>Selecciona una materia:</h1>
-	<button onclick="window.location.href='recibo_registron.php?nom_asign=Matemáticas'">Matemáticas</button>
-	<button onclick="window.location.href='recibo_registron.php?nom_asign=Ciencia'">Ciencia</button>
-	<button onclick="window.location.href='recibo_registron.php?nom_asign=Química'">Química</button>
-</body>
-</html>
-```
-
-Página de recibo_registron:
-```html
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Recibo de Registro</title>
-</head>
-<body>
-	<h1>Registros de la materia <?php echo $_GET['nom_asign']; ?>:</h1>
-	<table>
-		<tr>
-			<th>ID</th>
-			<th>Asignatura</th>
-		</tr>
-		<?php
-			//Obtener el nombre de la asignatura del parámetro de solicitud GET
-			$nom_asign = $_GET['nom_asign'];
-
-			//Conectar a la base de datos
-			$conn = new mysqli($servername, $username, $password, $dbname);
-
-			//Verificar la conexión
-			if ($conn->connect_error) {
-			  die("Connection failed: " . $conn->connect_error);
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"> </script>
+		<script src="consulta_asignatura.php"> </script> <!-- incluimos el archivo consulta_asignatura.php -->
+		<script>
+			function obtenerAsignatura(nom_asign)
+			{
+				$.ajax({
+					url: "consulta_asignatura.php",
+					type: "POST",
+					data: { nom_asign: nom_asign },
+					success: function(data) {
+						// Redirigir a la interfaz recibo_registron y mostrar los datos de la asignatura
+						window.location.href = "recibir_datos.php?datos=" + encodeURIComponent(data);
+					}
+				});
 			}
+		</script>
+	</body>
+</html>'''
 
-			//Hacer una consulta para obtener todos los registros correspondientes a la asignatura seleccionada
-			$sql = "SELECT * FROM tasignatura WHERE asignar = '$nom_asign'";
-			$result = $conn->query($sql);
+consulta_asignatura.php:
+'''<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "db_idac";
 
-			//Cerrar la conexión
-			$conn->close();
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-			//Mostrar los resultados de la consulta en la tabla
-			if ($result->num_rows > 0) {
-			  	while($row = $result->fetch_assoc()) {
-			  		echo "<tr><td>".$row["Id-materia"]."</td><td>".$row["asignar"]."</td></tr>";
-			  	}
-			} else {
-				echo "<tr><td colspan='2'>No hay registros para esta asignatura.</td></tr>";
-			}
-		?>
-	</table>
-</body>
-</html>
-```
+if ($conn->connect_error) {
+	die("Connection failed: " . $conn->connect_error);
+}
 
-Espero que esto te ayude en tu proyecto. ¡Buena suerte!
+// Función que retorna los datos de una asignatura
+function obtenerAsignatura($nom_asign)
+{
+	global $conn;
+
+	$sql = "SELECT * FROM 'tasignatura' WHERE nom_asign ='$nom_asign'";
+	$result = $conn->query($sql);
+
+	if ($result->num_rows > 0) {
+		// Retornar los datos de la asignatura en un arreglo asociativo
+		return $result->fetch_assoc();
+	} else {
+		return null;
+	}
+
+	$conn->close();
+}
+?> '''
+
+recibir_datos.php:
+'''<?php
+if (isset($_GET['datos'])) {
+	// Obtener los datos de la asignatura desde la URL
+	$asignatura = json_decode(urldecode($_GET['datos']), true);
+
+	if ($asignatura != null) {
+		// Mostrar los datos de la asignatura en la interfaz
+		echo "Nombre de la asignatura: " . $asignatura['nom_asign'] . "<br>";
+		
+		// ...
+	} else {
+		echo "No se encontraron datos para la asignatura especificada.";
+	}
+}
+?>
+'''
