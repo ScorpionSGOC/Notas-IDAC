945499a25588caf55c68beec2e037ffbcb500cc4
ScorpionSGOC <112102532+ScorpionSGOC@users.noreply.github.com>
2023-04-21 22:41:24 -0600

    Update Notas.php

--- Notas.php
+++ Notas.php
@@ -1,31 +1,43 @@
-Se puede crear un botón personalizado con un símbolo de más en medio utilizando HTML y CSS de la siguiente manera:
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title></title>
+		<style>
+			.btn {
+				display: inline-block;
+				padding: 8px 16px;
+				font-size: 16px;
+				font-weight: bold;
+				text-align: center;
+				color: #ffffff;
+				background-color: #3366cc;
+				border: none;
+				border-radius: 4px;
+				cursor: pointer;
+			}
+			
+			#button1 {
+				width: 100px;
+				height: 50px;
+				background-color: blue;
+				border: none;
+				border-radius: 5px;
+				color: white;
+				font-size: 30px;
+				cursor: pointer;
+			}
 
-HTML:
-```html
-<button id="button1"><span>+</span></button>
-```
-
-CSS:
-```css
-#button1 {
-  width: 100px;
-  height: 50px;
-  background-color: blue;
-  border: none;
-  border-radius: 5px;
-  color: white;
-  font-size: 30px;
-  cursor: pointer;
-}
-
-#button1 span {
-  display: inline-block;
-  vertical-align: middle;
-}
-```
-
-El código HTML crea un botón con un ID único de "button1" y un elemento de span dentro del botón que contiene el símbolo de más.
-
-El CSS establece el ancho, la altura, el color de fondo, el borde, el borde de radio, el color del texto, el tamaño de la fuente y el cursor para el botón.
-
-El CSS también establece la visualización en línea de bloque para el elemento de span y la alineación vertical en el centro para que el símbolo "+" aparezca en el centro del botón.
+			#button1 span {
+				display: inline-block;
+				vertical-align: middle;
+			}
+			
+		</style>
+	</head>
+	<body>
+	
+		<button id="button1"><span>+</span></button>
+		
+	</body>
+</html>
