2b95e51eca0f470b5ea3abb054446b08f2259946
ScorpionSGOC <112102532+ScorpionSGOC@users.noreply.github.com>
2023-04-21 22:55:47 -0600

    Actualizar Nota_2.php

--- Nota_2.php
+++ Nota_2.php
@@ -1,1 +1,54 @@
-fd
+<!DOCTYPE html> 
+ <html> 
+         <head> 
+               <meta charset="utf-8"> 
+               <title></title> 
+               <style> 
+                       .btn { 
+                               display: inline-block; 
+                               padding: 8px 16px; 
+                               font-size: 16px; 
+                               font-weight: bold; 
+                               text-align: center; 
+                               color: #ffffff; 
+                               background-color: #3366cc; 
+                               border: none; 
+                               border-radius: 4px; 
+                               cursor: pointer; 
+                       } 
+                        
+                       #button1 { 
+                               width: 100px; 
+                               height: 50px; 
+                               background-color: blue; 
+                               border: none; 
+                               border-radius: 5px; 
+                               color: white; 
+                               font-size: 30px; 
+                               cursor: pointer;
+                               display: flex;
+                               align-items: center;
+                               justify-content: center;
+                       } 
+
+                       #button1::before {
+                               content: "Materia";
+                               display: block;
+                               font-size: 16px;
+                               color: white;
+                               margin-bottom: 5px;
+                       }
+                        
+                       #button1 span { 
+                               display: inline-block; 
+                               vertical-align: middle; 
+                       } 
+                        
+               </style> 
+         </head> 
+         <body> 
+            
+               <button id="button1"><span>+</span></button> 
+                
+         </body> 
+ </html>
