59f6e0ca665e82ca33a9e5c05f86c2b3478fd673
ScorpionSGOC <112102532+ScorpionSGOC@users.noreply.github.com>
2023-04-22 20:55:47 -0600

    Actualizar login.php

--- login.php
+++ login.php
@@ -1,48 +1,38 @@
-El principal error del código es que la función obtenerAsignatura en el archivo consulta_asignatura.php 
-no se está llamando en ningún momento. Para solucionarlo, se debe agregar un código en el archivo consulta_asignatura.php 
-que permita llamar a la función obtenerAsignatura cuando se reciba el valor de nom_asign a través del método POST.
+Claro, aquí tienes un ejemplo básico de una interfaz con botones "Inicio", "Portafolio" y "Manual" utilizando Bootstrap. Asegúrate de incluir tanto Bootstrap CSS como JS en tu proyecto antes de copiar y pegar el siguiente código en el archivo HTML.
 
-Aquí está el código corregido para consulta_asignatura.php:
+1. Incluye las dependencias de Bootstrap en tu archivo HTML, si aún no las has agregado:
 
-<?php 
-$servername = "localhost"; 
-$username = "root"; 
-$password = ""; 
-$dbname = "db_idac"; 
+```html
+<!doctype html>
+<html lang="es">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+Knujsl7/1L_dstPt3HV5fef9lprG7+zOi5Q5gebnC9WNMFKo" crossorigin="anonymous">
 
-$conn = new mysqli($servername, $username, $password, $dbname); 
+    <title>Mi Interfaz</title>
+  </head>
+  <body>
+  <!-- Aquí va el contenido -->
+  </body>
+</html>
+```
 
-if ($conn->connect_error) { 
-    die("Connection failed: " . $conn->connect_error); 
-} 
+2. Ahora puedes copiar y pegar el siguiente código para la barra lateral:
 
-// Función que retorna los datos de una asignatura 
-function obtenerAsignatura($nom_asign) 
-{ 
-    global $conn; 
+```html
+<!-- Barra lateral -->
+<div class="d-flex flex-column bd-highlight position-fixed p-3 bg-light" style="width: 200px;">
+  <h2>Menú</h2>
+  <a class="btn btn-primary mt-2" href="#inicio" role="button">Inicio</a>
+  <a class="btn btn-primary mt-2" href="#portafolio" role="button">Portafolio</a>
+  <a class="btn btn-primary mt-2" href="#manual" role="button">Manual</a>
+</div>
+```
 
-    $sql = "SELECT * FROM tasignatura WHERE nom_asign ='$nom_asign'"; 
-    $result = $conn->query($sql); 
+Este código crea una barra lateral con una lista de botones para navegar entre "Inicio", "Portafolio" y "Manual". Puedes personalizar el CSS y la estructura según tus preferencias.
 
-    if ($result->num_rows > 0) { 
-        // Retornar los datos de la asignatura en un arreglo asociativo 
-        return $result->fetch_assoc(); 
-    } else { 
-        $conn->close(); 
-        return null; 
-    } 
-} 
-
-// Llamada a la función obtenerAsignatura 
-if (isset($_POST['nom_asign'])) {
-    $asignatura = obtenerAsignatura($_POST['nom_asign']);
-    if ($asignatura != null) {
-        // Devolver los datos de la asignatura en formato JSON
-        echo json_encode($asignatura);
-    } else {
-        echo "No se encontraron datos para la asignatura especificada.";
-    }
-} 
-?> 
-
-Se agregó el código para llamar a la función obtenerAsignatura y devolver los datos de la asignatura en formato JSON. Además, se verificó si el valor de $asignatura es nulo y se devolvió un mensaje de error correspondiente en caso de que la asignatura no se encuentre en la base de datos.
+Nota: Si prefieres, también puedes utilizar un componente "nav" de Bootstrap o adaptar la barra lateral para que sea responsive según el diseño de tu sitio.
