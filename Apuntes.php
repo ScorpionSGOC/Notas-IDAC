ec7d1201fc0bec1af707c2822edbb231a073df0c
ScorpionSGOC <112102532+ScorpionSGOC@users.noreply.github.com>
2023-04-24 11:42:17 -0600

    Update and rename Apuntes to Apuntes.php

---
+++ Apuntes.php
@@ -0,0 +1,33 @@
+<style>
+#imglogotec{
+				width: 10%;
+				height: 50%;
+				
+			}
+
+			h1{
+				color: #fff;
+			}
+			.txtitsm{
+				font-size: 40px;
+				color: #fff;
+			}
+			.menu{
+				background-color: #1A1818;
+				display: flex;
+				text-align: center;
+				height: 200px;
+				justify-content: center;
+			}
+</style>
+
+<body>
+<div class="menu">
+		<img id="imglogotec" src="/proyectoIDAC1/Imagenes/Logo Tec.png" alt="img" />
+		<div class="txtitsm">
+			<label>TecNM Campus San Marcos</label>
+		</div>
+		<br>
+		<h1>Aplicación web IDAC</h1>
+	</div>
+    </body>
