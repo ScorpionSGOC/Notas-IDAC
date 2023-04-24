afffc812fdcc5a4042d0e50e9a4602f54b78aad7
ScorpionSGOC <112102532+ScorpionSGOC@users.noreply.github.com>
2023-04-23 22:40:54 -0600

    Create Nota.php

---
+++ Nota.php
@@ -0,0 +1,24 @@
+div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
+								<div class="accordion-body">
+									<button class="btn btn-primary custom-button">
+										<span class="d-block">Texto superior</span>
+										<span class="d-block">+</span>
+									</button>
+									<button class="btn btn-primary custom-button">
+										<span class="d-block">Texto superior</span>
+										<span class="d-block">+</span>
+									</button>
+									<button class="btn btn-primary custom-button">
+										<span class="d-block">Texto superior</span>
+										<span class="d-block">+</span>
+									</button>
+									<button class="btn btn-primary custom-button">
+										<span class="d-block">Texto superior</span>
+										<span class="d-block">+</span>
+									</button>
+									<button class="btn btn-primary custom-button">
+										<span class="d-block">Texto superior</span>
+										<span class="d-block">+</span>
+									</button>
+								</div>
+							</div>
