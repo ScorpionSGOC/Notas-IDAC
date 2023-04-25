173101f0a430cf29bb8ec8b7f114f6a02f200a97
ScorpionSGOC <112102532+ScorpionSGOC@users.noreply.github.com>
2023-04-24 20:14:55 -0600

    Update Apuntes_ing.php

--- Apuntes_ing.php
+++ Apuntes_ing.php
@@ -22,19 +22,19 @@
 
 1.- I am currently reading an English book.
 2.- She is singing her favorite song in the shower.
-3.- They are dancing to the rhythm if the music.
-4.- Hi is eating a sandwich for his lunch.
+3.- They are dancing to the rhythm of the music.
+4.- He is eating a sandwich for his lunch.
 5.- We are swimming in the pool to cool down
 6.- She is making a delicious cake.
 7.- I am doing my homework before going out.
 8.- He is watching a movie on Netflix.
-9.- She is drinking a cup of the coffe in the morning.
+9.- She is drinking a cup of the coffee in the morning.
 10.- They are playing basketball at the playground.
 11.- She is running around the park.
 12.- He is sleeping peacefully on the bed.
-13.- She Is wearing a blue dress to the party tonight.
+13.- She is wearing a blue dress to the party tonight.
 14.- They are flying to the Europe for their summer vacation.
-15.- She us shopping for new shoes at the mall. 
+15.- She is shopping for new shoes at the mall. 
 
 
 
