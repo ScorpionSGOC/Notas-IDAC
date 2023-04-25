e933bc905eb969d82806d0e2b44b52db789dbe9e
ScorpionSGOC <112102532+ScorpionSGOC@users.noreply.github.com>
2023-04-25 12:34:37 -0600

    Actualizar Nota.php

--- Nota.php
+++ Nota.php
@@ -1,87 +1,85 @@
-Entiendo que deseas crear una interfaz para asignar materias a un usuario en una base de datos MariaDB y que al seleccionar una materia, esta se muestre en una nueva interfaz indicando la asignatura y materia seleccionada. A continuación, te proporciono un esbozo básico de cómo podrías desarrollarlo utilizando HTML, JavaScript y PHP para manejar la interacción con la base de datos MariaDB.
+Para mostrar una materia en una tabla HTML desde MaríaDB, necesitas utilizar un lenguaje de programación del lado del servidor como PHP para recuperar los datos y mostrarlos en una tabla HTML. A continuación, te presento un ejemplo de cómo puedes hacer esto utilizando PHP, HTML y SQL:
 
-1. Crea un archivo `index.html` que contendrá la interfaz con los botones de las materias:
+Paso 1: Conectar PHP con la base de datos MariaDB
 
-```html
+Crea el archivo `conexion.php` con el siguiente contenido:
+```php
+<?php
+$servername = "localhost";
+$username = "tu_usuario";
+$password = "tu_contraseña";
+$dbname = "nombre_de_tu_base_de_datos";
+
+// Crear la conexión
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verificar la conexión
+if ($conn->connect_error) {
+    die("Fallo de la conexión: " . $conn->connect_error);
+}
+?>
+```
+No olvides llenar las variables con tus propios datos de conexión.
+
+Paso 2: Crear un archivo que muestre la información en una tabla HTML
+
+Crea un archivo PHP llamado `mostrar_datos.php` con el siguiente contenido:
+
+```php
 <!DOCTYPE html>
-<html lang="es">
+<html>
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Asignación de materias</title>
+    <title>Mostrar Materias</title>
+    <style>
+        table {
+            border-collapse: collapse;
+            width: 100%;
+        }
+        th, td {
+            border: 1px solid black;
+            padding: 8px;
+            text-align: left;
+        }
+    </style>
 </head>
 <body>
-  <h1>Asignación de materias a usuarios</h1>
-  <p>Selecciona una materia:</p>
-  <button onclick="mostrarMateria('matematica')">Matemática</button>
-  <button onclick="mostrarMateria('quimica')">Química</button>
-  <button onclick="mostrarMateria('ciencia')">Ciencia</button>
 
-  <script>
-    function mostrarMateria(materia) {
-      // Añade aquí el código para llamar al archivo 'mostrarMateria.php' con la materia seleccionada, normalmente utilizando AJAX.
+<?php
+// Incluir el archivo de conexión
+require_once("conexion.php");
+
+// Obtener el RFC del usuario
+$rfc = "tu_rfc";
+
+// Crear SQL para obtener materias
+$sql = "SELECT m.clave, m.nombre_materia
+        FROM usuarios_materias um
+        JOIN materias m ON um.clave_asignatura = m.clave
+        WHERE um.RFC = '$rfc'";
+
+// Ejecutar la consulta
+$result = $conn->query($sql);
+
+// Verificar si hay resultados
+if ($result->num_rows > 0) {
+    // Mostrar los datos en una tabla HTML
+    echo "<table>";
+    echo "<tr><th>Clave</th><th>Materia</th></tr>";
+    while($row = $result->fetch_assoc()) {
+        echo "<tr><td>" . $row["clave"] . "</td><td>" . $row["nombre_materia"] . "</td></tr>";
     }
-  </script>
+    echo "</table>";
+} else {
+    echo "No hay resultados";
+}
+$conn->close();
+?>
+
 </body>
 </html>
 ```
+No olvides reemplazar "tu_rfc" con el RFC del usuario que desees mostrar en la página.
 
-2. Crea un archivo `mostrarMateria.php` que manejará las consultas a la base de datos y mostrará los resultados en una nueva interfaz:
+En este ejemplo, se crea una tabla HTML que muestra la clave y el nombre de la materia para un RFC específico. Simplemente ejecuta el archivo `mostrar_datos.php` en un servidor PHP compatible, y deberías obtener los resultados en una tabla HTML.
 
-```php
-<?php
-// Configuración de la base de datos
-$servername = "tu_servidor";
-$username = "tu_usuario";
-$password = "tu_contraseña";
-$dbname = "nombre_base_de_datos";
-
-// Crear conexión
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Checar conexión
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-
-// Sanitizar valor de 'materia' recibido por AJAX
-$materia = filter_input(INPUT_GET, 'materia', FILTER_SANITIZE_STRING);
-
-// Consulta a la tabla Usuario y Asignatura
-$sql = "SELECT u.RFC, a.claveAsig, a.nombre
-        FROM usuario u, asignatura a
-        WHERE u.RFC = a.RFC AND a.nombre = '{$materia}'";
-
-$result = $conn->query($sql);
-
-// Crear una tabla con los resultados
-if ($result->num_rows > 0) {
-  echo "<table><tr><th>RFC</th><th>Clave Asignatura</th><th>Asignatura</th></tr>";
-  while ($row = $result->fetch_assoc()) {
-    echo "<tr><td>" . $row["RFC"] . "</td><td>" . $row["claveAsig"] . "</td><td>" . $row["nombre"] . "</td></tr>";
-  }
-  echo "</table>";
-} else {
-  echo "No se encontraron resultados";
-}
-
-$conn->close();
-?>
-```
-
-3. En el archivo `index.html`, utiliza AJAX para llamar al archivo `mostrarMateria.php` con la referencia de la materia seleccionada:
-
-```javascript
-function mostrarMateria(materia) {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      document.querySelector("body").innerHTML = this.responseText;
-    }
-  };
-  xhttp.open("GET", "mostrarMateria.php?materia=" + materia, true);
-  xhttp.send();
-}
-```
-
-Este es un ejemplo básico para darte una idea. Recuerda que debes adaptarlo según tus necesidades y requerimientos específicos, además de añadir las medidas de seguridad necesarias (por ejemplo, consultas preparadas para evitar inyección SQL).
+Asegúrate de adaptar los nombres de las tablas y columnas en el SQL de acuerdo con tu esquema de base de datos específico.
