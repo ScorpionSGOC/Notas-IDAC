7b51b08d1b8fd755927c0cd8004da0fcae5317f0
ScorpionSGOC <112102532+ScorpionSGOC@users.noreply.github.com>
2023-04-24 19:47:22 -0600

    Update Apuntes_ing.php

--- Apuntes_ing.php
+++ Apuntes_ing.php
@@ -1,1 +1,22 @@
+    
+    TASK #1:
+
+    Write sentences using the following verbs. Use present continuous tense.
+
+    read
+    sing
+    dance
+    eat
+    swim
+    make
+    do
+    watch
+    drink
+    play
+    run
+    sleep
+    wear
+    fly
+    shop
+
 
