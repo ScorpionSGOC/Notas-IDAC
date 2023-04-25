4d865c2f80fea6962952f96752fdcd13a131eb83
ScorpionSGOC <112102532+ScorpionSGOC@users.noreply.github.com>
2023-04-24 20:17:42 -0600

    Actualizar Apuntes_ing.php

--- Apuntes_ing.php
+++ Apuntes_ing.php
@@ -34,7 +34,24 @@
 12.- He is sleeping peacefully on the bed.
 13.- She is wearing a blue dress to the party tonight.
 14.- They are flying to the Europe for their summer vacation.
-15.- She is shopping for new shoes at the mall. 
+15.- She is shopping for new shoes at the mall.
+
+
+1. Aí am kúrrentli rídin an ínglish buk.
+2. Shi is sínɡin her féivorit són in de sháuer.
+3. Dei ar dánsin tu de rídm of de miusic.
+4. Hi is ítin a sándwich for his lanch.
+5. Wi ar swimin in de pul tu kul daun.
+6. Shi is méikin a dilíshas kéik.
+7. Ai am dóin mai háumwórk bifor ɡóin aut.
+8. Hi is wóchin a múvi on Netfliks.
+9. Shi is drinkin a kap of de kofi in de mórniŋ.
+10. Dei ar pléiin básketbol at de pléɡraund.
+11. Shi is ranin aráund de park.
+12. Hi is slípin písfuli on de bed.
+13. Shi is werin a blu drés tu de párti tunáit.
+14. Dei ar fláin tu de Yuróp for déir samer vakeishon.
+15. Shi is shopin for niú shuz at de mol. 
 
 
 
