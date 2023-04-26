e4fd88837f0dc76c018d66fbd09c394a783b2987
ScorpionSGOC <112102532+ScorpionSGOC@users.noreply.github.com>
2023-04-25 23:31:03 -0600

    Actualizar Apuntes_ing.php

--- Apuntes_ing.php
+++ Apuntes_ing.php
@@ -1,70 +1,86 @@
-    
-    TASK #1:
+Para crear un botón en una página web que ejecute esta consulta, necesitarás combinar HTML, CSS y JavaScript para la interacción del usuario, junto con algún lenguaje de programación server-side (como PHP) y una base de datos (como MySQL) para gestionar la consulta en sí. Aquí hay un ejemplo de cómo puedes hacerlo usando PHP y MySQL:
 
-    Write sentences using the following verbs. Use present continuous tense.
+1. Crea un archivo HTML (`index.html`):
 
-    read
-    sing
-    dance
-    eat
-    swim
-    make
-    do
-    watch
-    drink
-    play
-    run
-    sleep
-    wear
-    fly
-    shop.
+```html
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Consulta con botón</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <form action="consulta.php" method="post">
+        <input type="text" name="rfc" placeholder="Introduce el RFC deseado" required>
+        <button type="submit" name="consultar">Consultar</button>
+    </form>
+    <div id="resultado"></div>
+    <script src="main.js"></script>
+</body>
+</html>
+```
 
-                              Monday, April 24th, 2023. Osvaldo Cruz
-TASK 1:
+2. Crea un archivo CSS (`styles.css`) para darle estilo al botón:
 
+```css
+button {
+    background-color: #4CAF50;
+    color: white;
+    padding: 10px 20px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin: 4px 2px;
+    cursor: pointer;
+    border: none;
+}
+```
 
-1.- I am reading an English book.
-2.- She is singing her favorite song in the shower.
-3.- They are dancing to the rhythm of the music.
-4.- He is eating a sandwich for lunch.
-5.- We are swimming in the pool to cool down
-6.- She is making a delicious cake.
-7.- I am doing my homework before going out.
-8.- He is watching a movie on Netflix.
-9.- She is drinking a cup of the coffee in the morning.
-10.- They are playing basketball at the playground.
-11.- She is running around the park.
-12.- He is sleeping peacefully on the bed.
-13.- She is wearing a blue dress
-14.- They are flying to Europe.
-15.- She is shopping for new shoes at the mall.
+3. Crear un archivo PHP (`consulta.php`) que se encargue de realizar la consulta a la base de datos:
 
+```php
+<?php
+// Conexión a la base de datos
+$host = "localhost";
+$user = "tu_usuario";
+$pass = "tu_contraseña";
+$db = "nombre_de_la_bd";
 
+$conn = new mysqli($host, $user, $pass, $db);
 
+if ($conn->connect_error) {
+    die("Error de conexión: " . $conn->connect_error);
+}
 
+// Obtener el RFC del formulario
+$rfc = $_POST['rfc'];
 
+// Consulta SQL
+$sql = "SELECT usuario.rfc, usuario.Nombre, tasignatura.claveasig, tasignatura.materia
+FROM usuario
+JOIN c_sig_usu ON usuario.rfc = c_sig_usu.rfc
+JOIN tasignatura ON c_sig_usu.claveasig = tasignatura.claveasig
+WHERE usuario.rfc = '$rfc'";
 
+$result = $conn->query($sql);
 
+// Procesar y mostrar resultados
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        echo "RFC: " . $row["rfc"] . " - Nombre: " . $row["Nombre"] . " - Clave asignatura: " . $row["claveasig"] . " - Materia: " . $row["materia"] . "<br>";
+    }
+} else {
+    echo "No se encontraron resultados";
+}
 
+$conn->close();
+?>
+```
 
+Por favor, asegúrate de reemplazar valores como `tu_usuario`, `tu_contraseña`, y `nombre_de_la_bd` con tus propios datos de conexión a la base de datos MySQL.
 
+Con este ejemplo lograrás que, al ingresar un RFC y presionar el botón "Consultar", se realice la consulta indicada y los resultados se muestren en la página.
 
-
-1. Aí am kúrrentli rídin an ínglish buk.
-2. Shi is sínɡin her féivorit són in de sháuer.
-3. Dei ar dánsin tu de rídm of de miusic.
-4. Hi is ítin a sándwich for his lanch.
-5. Wi ar swimin in de pul tu kul daun.
-6. Shi is méikin a dilíshas kéik.
-7. Ai am dóin mai háumwórk bifor ɡóin aut.
-8. Hi is wóchin a múvi on Netfliks.
-9. Shi is drinkin a kap of de kofi in de mórniŋ.
-10. Dei ar pléiin básketbol at de pléɡraund.
-11. Shi is ranin aráund de park.
-12. Hi is slípin písfuli on de bed.
-13. Shi is werin a blu drés tu de párti tunáit.
-14. Dei ar fláin tu de Yuróp for déir samer vakeishon.
-15. Shi is shopin for niú shuz at de mol. 
-
-
-
+Ten en cuenta que este es solo un ejemplo básico, y deberías mejorar este código siguiendo las buenas prácticas de seguridad y manejo de errores al implementarlo en proyectos reales.
