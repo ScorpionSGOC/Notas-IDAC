a11d7de203cf81dc5255ab9c2d8f1ca70d813bbf
ScorpionSGOC <112102532+ScorpionSGOC@users.noreply.github.com>
2023-04-26 09:56:09 -0600

    Actualizar Texto.php

--- Texto.php
+++ Texto.php
@@ -1,48 +1,82 @@
-Puedes agregar el texto de "Agregar"  que se encuentra en el botón en la parte superior del diseño:
-como se crea un botón personalizado con un símbolo de mas en medio, con html y ccs.
+Primero, para ejecutar una consulta específica en SQL a través de un archivo PHP, debes crear un archivo PHP que acepte un parámetro y realice la consulta. Vamos a llamar a este archivo `buscar_asignaturas.php`.
+
+Dentro del archivo `buscar_asignaturas.php`:
+
+```php
+<?php
+$rfc = $_GET['rfc'];
+
+$conexion = new mysqli('localhost', 'username', 'password', 'database');
+
+if ($conexion->connect_error) {
+    die('Error de conexión: ' . $conexion->connect_error);
+}
+
+$sql = "SELECT usuario.rfc, usuario.Nombre, tasignatura.claveasig, tasignatura.materia
+        FROM usuario
+        JOIN c_sig_usu ON usuario.rfc = c_sig_usu.rfc
+        JOIN tasignatura ON c_sig_usu.claveasig = tasignatura.claveasig
+        WHERE usuario.rfc = ?";
+
+$consulta = $conexion->prepare($sql);
+$consulta->bind_param('s', $rfc);
+$consulta->execute();
+
+$resultado = $consulta->get_result();
+
+while ($row = $resultado->fetch_assoc()) {
+    echo 'Nombre: ' . $row['Nombre'] . ', Materia: ' . $row['materia'] . '<br>';
+}
+
+$consulta->close();
+$conexion->close();
+?>
+```
+
+Luego, en el archivo `index.html`, debes agregar un campo de entrada y un botón para que puedas enviar el "RFC" deseado al archivo `buscar_asignaturas.php`. Aquí tienes el código modificado:
+
+```
 <!DOCTYPE html>
 <html>
-	<head>
-		<meta charset="utf-8">
-		<title></title>
-		<style>
-			.btn {
-				display: inline-block;
-				padding: 8px 16px;
-				font-size: 16px;
-				font-weight: bold;
-				text-align: center;
-				color: #ffffff;
-				background-color: #3366cc;
-				border: none;
-				border-radius: 4px;
-				cursor: pointer;
-			}
-			
-			.btn::before {
-				content: "+";
-				display: inline-block;
-				width: 20px;
-				height: 20px;
-				line-height: 20px;
-				font-size: 14px;
-				font-weight: bold;
-				text-align: center;
-				margin-right: 10px;
-				background-color: #ffffff;
-				border-radius: 50%;
-				color: #3366cc;
-				box-shadow: 0 0 0 2px #3366cc, 0 0 0 4px #ffffff;
-			}
+        <head>
+                <meta charset="utf-8">
+                <title>Consulta de Asignaturas</title>
+                <style>
+                        body {
+                                font-family: Arial, sans-serif;
+                        }
 
-			.btn-text {
-				margin-right: 5px;
-			}
-			
-		</style>
-	</head>
-	<body>
-		<button class="btn">Agregar</button>
-		
-	</body>
+                        .button-container {
+                                display: flex;
+                                justify-content: center;
+                                gap: 10px;
+                        }
+
+                        button {
+                                padding: 10px 20px;
+                                font-size: 16px;
+                                cursor: pointer;
+                        }
+
+                        .input-container {
+                            display: flex;
+                            justify-content: center;
+                        }
+                </style>
+        </head>
+        <body>
+                <h1>Seleccione una asignatura</h1>
+                <form action="buscar_asignaturas.php">
+                    <div class="input-container">
+                        <label for="rfc">Ingrese el RFC:</label>
+                        <input type="text" name="rfc" id="rfc" required>
+                    </div>
+                    <div class="button-container">
+                        <button type="submit">Buscar asignaturas</button>
+                    </div>
+                </form>
+        </body>
 </html>
+```
+
+Después de realizar las modificaciones, copia y pega el código de cada archivo en sus respectivos archivos y asegúrate de actualizar las credenciales de la base de datos y el nombre de la base de datos en el archivo `buscar_asignaturas.php`.
