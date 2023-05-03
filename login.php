43c2d18f41ca672c1f1375943bed7b7247f9c0db
ScorpionSGOC <112102532+ScorpionSGOC@users.noreply.github.com>
2023-05-02 23:40:59 -0600

    Actualizar login.php

--- login.php
+++ login.php
@@ -1,116 +1,62 @@
-Para cumplir con ese requerimiento, necesitarás utilizar HTML, CSS, JavaScript y PHP. Voy a mostrarte un ejemplo básico que incluye todos los componentes que mencionaste:
+¡Por supuesto! Aquí hay un ejemplo de cómo se podría crear un formulario para registrar las calificaciones de un alumno:
 
-1. Primero, crea un archivo `index.html` con el siguiente contenido:
-
-```html
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Seleccionar Asignatura</title>
-    <link rel="stylesheet" href="styles.css">
-</head>
-<body>
-    <h1>Seleccione una asignatura:</h1>
-    <div class="button-container">
-        <button data-asignatura="matemática">Matemática</button>
-        <button data-asignatura="química">Química</button>
-        <button data-asignatura="ciencia">Ciencia</button>
-    </div>
-
-    <script src="scripts.js"></script>
-</body>
-</html>
+HTML:
+```
+<form>
+  <label for="student-name">Nombre del estudiante:</label>
+  <input type="text" id="student-name" name="student-name" required>
+  
+  <label for="subject">Materia:</label>
+  <select id="subject" name="subject" required>
+    <option value="">Selecciona una materia</option>
+    <option value="matematicas">Matemáticas</option>
+    <option value="ciencias">Ciencias</option>
+    <option value="historia">Historia</option>
+  </select>
+  
+  <label for="grade">Calificación:</label>
+  <input type="number" id="grade" name="grade" min="0" max="10" step="0.1" required>
+  
+  <button type="submit">Guardar calificación</button>
+</form>
 ```
 
-2. Crea un archivo `styles.css` para agregar estilos básicos a los botones:
-
-```css
-body {
-    font-family: Arial, sans-serif;
+CSS:
+```
+form {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 20px;
 }
 
-.button-container {
-    display: flex;
-    justify-content: center;
-    gap: 10px;
+label {
+  margin-top: 10px;
 }
 
-button {
-    padding: 10px 20px;
-    font-size: 16px;
-    cursor: pointer;
+input[type=text], select, input[type=number] {
+  width: 100%;
+  padding: 12px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+  margin-top: 6px;
+  margin-bottom: 16px;
+}
+
+button[type=submit] {
+  background-color: #4CAF50;
+  color: white;
+  padding: 12px 20px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  transition: background-color 0.3s;
+}
+
+button[type=submit]:hover {
+  background-color: #3e8e41;
 }
 ```
 
-3. A continuación, crea un archivo `scripts.js` para agregar interacción a los botones:
-
-```javascript
-const buttons = document.querySelectorAll('button');
-
-buttons.forEach(button => {
-    button.addEventListener('click', () => {
-        const asignatura = button.getAttribute('data-asignatura');
-        window.location.href = `mostrar_asignatura.php?asignatura=${asignatura}`;
-    });
-});
-```
-
-4. Finalmente, crea un archivo `mostrar_asignatura.php` que recupere el dato enviado y muestre la asignatura seleccionada en una tabla. Asegúrate de insertar el nombre de las asignaturas en la tabla 'tasignatura' en tu base de datos de MariaDB.
-
-```php
-<?php
-$asignatura = $_GET['asignatura'];
-
-// Conexión a la base de datos (cambia los valores según tus credenciales)
-$conn = new mysqli('localhost', 'user', 'password', 'nombre_base_de_datos');
-
-// Verifica la conexión
-if ($conn->connect_error) {
-    die("Error en la conexión: " . $conn->connect_error);
-}
-
-// Consulta SQL para obtener el nom_asign
-$sql = "SELECT nom_asign FROM tasignatura WHERE nom_asign = '$asignatura'";
-$result = $conn->query($sql);
-$nombre_asignatura = "";
-
-if ($result->num_rows > 0) {
-    while ($row = $result->fetch_assoc()) {
-        $nombre_asignatura = $row['nom_asign'];
-    }
-} else {
-    echo "No se encontró la asignatura";
-}
-$conn->close();
-?>
-
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Asignatura Seleccionada</title>
-    <style>
-        table, th, td {
-            border: 1px solid black;
-            border-collapse: collapse;
-        }
-    </style>
-</head>
-<body>
-    <h1>Asignatura Seleccionada</h1>
-    <table>
-        <tr>
-            <th>Asignatura</th>
-        </tr>
-        <tr>
-            <td><?php echo $nombre_asignatura; ?></td>
-        </tr>
-    </table>
-</body>
-</html>
-```
-
-Con estos archivos, tendrás la funcionalidad básica que mencionaste. Asegúrate de configurar correctamente la conexión a la base de datos y de tener un servidor PHP para ejecutar el código.
+Este formulario incluye campos para el nombre del estudiante, la materia y la calificación, y requiere que todos los campos estén completados antes de enviar el formulario. El botón de enviar está estilizado con CSS para que tenga un color de fondo verde y tenga un efecto de transición al pasar el cursor sobre él. ¡Espero que esto ayude!
