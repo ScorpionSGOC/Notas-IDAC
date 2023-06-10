b64fdf71c8045c988de21f3587ede2d8c97a481b
ScorpionSGOC <112102532+ScorpionSGOC@users.noreply.github.com>
2023-06-10 10:59:47 -0600

    Update Notas.php

--- Notas.php
+++ Notas.php
@@ -1,56 +1,66 @@
-Para crear un botón personalizado con un símbolo de "+" en la parte inferior y un texto en la parte superior utilizando Bootstrap, sigue estos pasos:
+Cuales son las clases del siguiente codigo:
+<?php
+require_once('config/conexion.php');
 
-1. Asegúrate de incluir las dependencias necesarias de Bootstrap en tu archivo HTML (CSS y JS):
 
-```html
-<!doctype html>
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $usuario = $_POST["rfc"];
+    $contraseña = $_POST["password"];
+
+    if (validarUsuario($usuario, $contraseña)) {
+        // El usuario y la contraseña son <válidos></válidos>
+        exit();
+    } 
+}
+?>
+<!DOCTYPE html>
 <html lang="es">
+
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/IDAC/css/estyle.css">
+    <title>Login</title>
 </head>
+<?php require_once ('./componentes/header.php')?>
 <body>
-  <!-- Aquí va tu código HTML -->
+    
+    <section class="flex-container">
+        <div class="caja">
+        <form method="post" action="index.php">
+    <br><br>
+    <h1>Inicio de Sesión</h1>
+    <br>
+    <label>RFC:</label>
+    <input type="text" name="rfc" required>
+    <br>
+    <label>Contraseña:</label>
+    <input type="password" name="password" id="password" required>
+    <br>
+    <input id="showPass" class="check" type="checkbox" onclick="mostrarPassword()"><label for="showPass">Mostrar contraseña</label>
+    <br>
+    <input class="btniniciar" type="submit" value="Iniciar sesión">
+</form>
+
+<script>
+    function mostrarPassword() {
+        var x = document.getElementById("password");
+        if (x.type === "password") {
+            x.type = "text";
+        } else {
+            x.type = "password";
+        }
+    }
+</script>
+
+
+        </div>
+    </section>
 </body>
-</html>
-```
-
-2. Ahora crea el botón personalizado utilizando el sistema de grid de Bootstrap y clases de botón, aquí un ejemplo:
-
-```html
-<div class="container mt-5">
-  <div class="row">
-    <div class="col">
-      <button class="btn btn-primary custom-button">
-        <span class="d-block">Texto superior</span>
-        <span class="d-block">+</span>
-      </button>
-    </div>
-  </div>
+<div id="final">
+    <?php  require_once('./componentes/footer.php');?>
+    <p>&copy; Copyright 2023 ITSM - Todos los Derechos Reservados</p>
 </div>
-```
 
-3. Puedes ajustar el estilo del botón añadiendo reglas CSS personalizadas en el elemento `<style>` en el encabezado o en un archivo de estilo aparte:
-
-```html
-<style>
-  .custom-button {
-    font-size: 24px;
-  }
-  .custom-button span:first-child {
-    font-size: 14px;
-    font-weight: bold;
-  }
-  .custom-button span:last-child {
-    font-size: 30px;
-    font-weight: bold;
-    margin-top: -5px;
-  }
-</style>
-```
-
-Con estos pasos, tendrás creado un botón personalizado con Bootstrap que tiene un símbolo de "+" en la parte inferior y un texto en la parte superior. Puedes ajustar el estilo y tamaño según lo necesites en las reglas CSS personalizadas.
+</html>
